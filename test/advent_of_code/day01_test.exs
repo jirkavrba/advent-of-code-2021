9159c57c3494f101cab0c9ae33fd0b09f45ef2b9
Jiří Vrba <jiri@vrba.dev>
2021-12-01 14:51:05 +0100

    Implement second task of day 1

--- test/advent_of_code/day01_test.exs
+++ test/advent_of_code/day01_test.exs
@@ -1,6 +1,6 @@
 defmodule AdventOfCode.Day01Test do
   use ExUnit.Case
-  alias AdventOfCode.Day01.Task1
+  alias AdventOfCode.Day01.{Task1, Task2}
 
   test "task 1 example" do
     report = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
@@ -13,5 +13,18 @@ defmodule AdventOfCode.Day01Test do
     |> Enum.map(&String.to_integer/1)
 
     assert Task1.compute_depth_increments(report) == 1451
+  end
+
+  test "task 2 example" do
+    report = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    assert Task2.compute_depth_increments(report) == 5
+  end
+
+  test "task 2 input" do
+    report = File.read!("priv/inputs/day01.txt")
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+
+    assert Task2.compute_depth_increments(report) == 1395
   end
 end
