f30955df931729314343a910f26609d4e0226bcd
Jiří Vrba <jiri@vrba.dev>
2021-12-01 14:37:28 +0100

    Implement the first task of day 1 in elixir

---
+++ test/advent_of_code/day01_test.exs
@@ -0,0 +1,17 @@
+defmodule AdventOfCode.Day01Test do
+  use ExUnit.Case
+  alias AdventOfCode.Day01.Task1
+
+  test "task 1 example" do
+    report = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    assert Task1.compute_depth_increments(report) == 7
+  end
+
+  test "task 1 input" do
+    report = File.read!("priv/inputs/day01.txt")
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+
+    assert Task1.compute_depth_increments(report) == 1451
+  end
+end
