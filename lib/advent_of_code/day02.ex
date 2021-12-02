f960bcc88e11f07d10bce6ecf983961c12085998
Jiří Vrba <jiri@vrba.dev>
2021-12-02 09:12:46 +0100

    Add day 2, task 2 implementation in elixir

--- lib/advent_of_code/day02.ex
+++ lib/advent_of_code/day02.ex
@@ -1,12 +1,17 @@
 defmodule AdventOfCode.Day02 do
-
   defmodule Submarine do
     @type t :: %__MODULE__{
-      x: integer(),
-      y: integer()
-    }
+            x: integer(),
+            y: integer(),
+            aim: integer()
+          }
 
-    defstruct x: 0, y: 0
+    defstruct x: 0, y: 0, aim: 0
+
+    @spec aim(t(), integer()) :: t()
+    def aim(%__MODULE__{aim: aim} = submarine, delta) when is_integer(delta) do
+      %__MODULE__{submarine | aim: aim + delta}
+    end
 
     @spec move(t(), %{x: integer(), y: integer()}) :: t()
     def move(%__MODULE__{x: x, y: y} = submarine, %{x: dx, y: dy}) do
@@ -58,10 +63,10 @@ defmodule AdventOfCode.Day02 do
     @spec calculate_destination_product(list(String.t())) :: integer()
     def calculate_destination_product(instructions) when is_list(instructions) do
       submarine = %Submarine{}
+
       reducer = fn instruction, submarine ->
         with [instruction, number] <- String.split(instruction, " "),
-                          distance <- String.to_integer(number)
-        do
+             distance <- String.to_integer(number) do
           case instruction do
             "forward" -> Submarine.move(submarine, %{x: distance, y: 0})
             "up"      -> Submarine.move(submarine, %{x: 0, y: -distance})
@@ -75,4 +80,59 @@ defmodule AdventOfCode.Day02 do
     end
   end
 
+  defmodule Task2 do
+    @moduledoc """
+    Based on your calculations, the planned course doesn't seem to make any sense.
+    You find the submarine manual and discover that the process is actually slightly more complicated.
+
+    In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at 0.
+     The commands also mean something entirely different than you first thought:
+
+      `down X` increases your aim by `X` units.
+      `up X` decreases your aim by `X` units.
+      `forward X` does two things:
+          - It increases your horizontal position by `X` units.
+          - It increases your depth by your aim multiplied by `X`.
+
+    Again note that since you're on a submarine, down and up do the opposite of what you might expect:
+     "down" means aiming in the positive direction.
+
+    Now, the above example does something different:
+
+    - `forward 5` adds 5 to your horizontal position, a total of 5. Because your aim is 0, your depth does not change.
+    - `down 5` adds 5 to your aim, resulting in a value of 5.
+    - `forward 8` adds 8 to your horizontal position, a total of 13. Because your aim is 5, your depth increases by 8*5=40.
+    - `up 3` decreases your aim by 3, resulting in a value of 2.
+    - `down 8` adds 8 to your aim, resulting in a value of 10.
+    - `forward 2` adds 2 to your horizontal position, a total of 15. Because your aim is 10, your depth increases by 2*10=20 to a total of 60.
+
+    After following these new instructions, you would have a horizontal position of 15 and a depth of 60.
+    (Multiplying these produces `900`.)
+
+    Using this new interpretation of the commands,
+    calculate the horizontal position and depth you would have after following the planned course.
+    What do you get if you multiply your final horizontal position by your final depth?
+    """
+    @spec calculate_destination_product(list(String.t())) :: integer()
+    def calculate_destination_product(instructions) when is_list(instructions) do
+      submarine = %Submarine{}
+
+      reducer = fn instruction, submarine ->
+        with [instruction, number] <- String.split(instruction, " "),
+             distance <- String.to_integer(number) do
+          case instruction do
+            "down"    -> Submarine.aim(submarine, distance)
+            "up"      -> Submarine.aim(submarine, -distance)
+            "forward" -> Submarine.move(submarine, %{
+              x: distance,
+              y: submarine.aim * distance
+            })
+          end
+        end
+      end
+
+      destination = Enum.reduce(instructions, submarine, reducer)
+      destination.x * destination.y
+    end
+  end
 end
--- test/advent_of_code/day02_test.exs
+++ test/advent_of_code/day02_test.exs
@@ -2,7 +2,7 @@ defmodule AdventOfCode.Day02Test do
   use ExUnit.Case
 
   alias AdventOfCode.Input
-  alias AdventOfCode.Day02.Task1
+  alias AdventOfCode.Day02.{Task1, Task2}
 
   test "task 1 example" do
     instructions = [
@@ -17,8 +17,26 @@ defmodule AdventOfCode.Day02Test do
     assert Task1.calculate_destination_product(instructions) == 150
   end
 
-  test "task 2 example" do
+  test "task 1 input" do
     instructions = Input.read_lines_from_file("priv/inputs/day02.txt")
     assert Task1.calculate_destination_product(instructions) == 1813801
+  end
+
+  test "task 2 example" do
+    instructions = [
+      "forward 5",
+      "down 5",
+      "forward 8",
+      "up 3",
+      "down 8",
+      "forward 2",
+    ]
+
+    assert Task2.calculate_destination_product(instructions) == 900
+  end
+
+  test "task 2 input" do
+    instructions = Input.read_lines_from_file("priv/inputs/day02.txt")
+    assert Task2.calculate_destination_product(instructions) == 1960569556
   end
 end
