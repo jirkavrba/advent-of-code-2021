357141dd4b00fc632e247ace589a1380a6316745
Jiří Vrba <jiri@vrba.dev>
2021-12-02 09:47:26 +0100

    Implement day 2, task 2 in kotlin

--- src/main/kotlin/dev/vrba/adventofcode/days/Day02.kt
+++ src/main/kotlin/dev/vrba/adventofcode/days/Day02.kt
@@ -3,8 +3,9 @@ package dev.vrba.adventofcode.days
 class Day02 {
     // TODO: Somehow figure out how to manage day descriptions
 
-    data class Submarine(val x: Int = 0, val y: Int = 0) {
+    data class Submarine(val x: Int = 0, val y: Int = 0, val aim: Int = 0) {
         fun move(x: Int, y: Int): Submarine = copy(x = this.x + x, y = this.y + y)
+        fun aim(d: Int): Submarine = copy(aim = this.aim + d)
     }
 
     fun task1(instructions: List<String>): Int {
@@ -25,5 +26,21 @@ class Day02 {
         return destination.x * destination.y
     }
 
-    fun task2(instructions: List<String>): Int = TODO()
+    fun task2(instructions: List<String>): Int {
+        val submarine = Submarine()
+        val destination = instructions.fold(submarine) {
+                submarine, raw ->
+            val (instruction, number) = raw.split(" ")
+            val distance = number.toInt()
+
+            when (instruction) {
+                "forward" -> submarine.move(distance, distance * submarine.aim)
+                "up" -> submarine.aim(-distance)
+                "down" -> submarine.aim(distance)
+                else -> throw IllegalArgumentException("Invalid instruction")
+            }
+        }
+
+        return destination.x * destination.y
+    }
 }
