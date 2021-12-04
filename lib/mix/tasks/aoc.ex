bcdecd63f3b0210c6037493a80b7347cb07ffea6
Jiří Vrba <jiri@vrba.dev>
2021-12-04 08:50:09 +0100

    Add mix aoc task for scaffolding days

---
+++ lib/mix/tasks/aoc.ex
@@ -0,0 +1,34 @@
+defmodule Mix.Tasks.Aoc do
+  use Mix.Task
+
+  @shortdoc "A simple mix task to scaffold AoC files structure for a day specified in the params."
+
+  @moduledoc ~S"""
+  For example, using `mix aoc 5` will generate the following files:
+  - `/lib/advent_of_code/day05.ex`
+  - `/test/advent_of_code/day05_test.ex`
+  - `/priv/inputs/day05.txt`
+  """
+
+  @impl Mix.Task
+  def run([]) do
+    Mix.shell().error("Missing day number as an argument!\nFor example, use 'mix aoc 5'")
+  end
+
+  @impl Mix.Task
+  def run([day | _]) do
+    day = String.pad_leading(day, 2, "0")
+
+    file_name = Macro.underscore("day#{day}")
+    module_name = Macro.camelize("day#{day}")
+
+    assigns = [
+      file_name: file_name,
+      module_fullname: Module.concat(AdventOfCode, module_name),
+      test_module_fullname:  Module.concat(AdventOfCode, module_name <> "Test"),
+    ]
+
+    Mix.Generator.copy_template("priv/mix/day.eex", "lib/advent_of_code/#{file_name}.ex", assigns)
+    Mix.Generator.copy_template("priv/mix/day_test.eex", "test/advent_of_code/#{file_name}_test.ex", assigns)
+  end
+end
