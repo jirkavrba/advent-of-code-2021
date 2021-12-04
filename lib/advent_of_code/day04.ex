9833330de361cc8c1115ea28a330490cc53c1309
Jiří Vrba <jiri@vrba.dev>
2021-12-04 10:14:03 +0100

    Implement day 4, task 1 in elixir

---
+++ lib/advent_of_code/day04.ex
@@ -0,0 +1,88 @@
+defmodule AdventOfCode.Day04 do
+  defmodule BingoBoard do
+    @moduledoc """
+    Bingo board struct, all columns and diagonals are counted as rows for simplicity
+    """
+    @type t :: %__MODULE__{
+      rows: list(list(integer())),
+      numbers: list(integer())
+    }
+    @enforce_keys [:rows, :numbers]
+    defstruct [:rows, :numbers]
+
+    @size 5
+
+    @spec size() :: integer()
+    def size(), do: @size
+
+    # Only supports 5x5 grid
+    @spec create(list(integer)) :: t()
+    def create(numbers) when is_list(numbers) and length(numbers) == 25 do
+      # TODO: Use NX and tensors for this...
+      rows = Enum.chunk_every(numbers, @size)
+      columns = for i <- 0..(@size - 1), do: Enum.map(rows, fn row -> Enum.at(row, i) end)
+
+      %__MODULE__{
+        numbers: numbers,
+        rows: rows ++ columns
+      }
+    end
+
+    @spec mark(t(), number) :: t()
+    def mark(%__MODULE__{rows: rows, numbers: numbers} = board, number) do
+      rows = Enum.map(rows, fn row -> row -- [number] end)
+      numbers = numbers -- [number]
+
+      %BingoBoard{board | rows: rows, numbers: numbers}
+    end
+
+    @spec won(t()) :: boolean()
+    def won(%BingoBoard{rows: rows}) do Enum.any?(rows, &Enum.empty?/1)
+    end
+
+    @spec score(t()) :: integer()
+    def score(%BingoBoard{numbers: numbers}), do: Enum.sum(numbers)
+  end
+
+  defmodule Task1 do
+    defp parse_boards(source) do
+      source
+      |> Enum.map(&String.trim/1)
+      |> Enum.join(" ")
+      |> String.split(" ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.chunk_every(BingoBoard.size() * BingoBoard.size())
+      |> Enum.map(&BingoBoard.create/1)
+    end
+
+    defp draw_next_number(number, {boards, nil, _}) do
+      boards = Enum.map(boards, fn board -> BingoBoard.mark(board, number) end)
+      winner = Enum.find(boards, &BingoBoard.won/1)
+
+      case winner do
+        nil -> {boards, nil, nil}
+        board -> {boards, board, number}
+      end
+    end
+
+    defp draw_next_number(_number, acc), do: acc
+
+    def find_score_of_winner_board(lines) when is_list(lines) do
+      [draws | boards] = lines
+      draws = draws
+        |> String.trim()
+        |> String.split(",")
+        |> Enum.map(&String.to_integer/1)
+
+      boards = parse_boards(boards)
+      initial = {boards, nil, nil}
+
+      draws
+      |> Enum.reduce(initial, &draw_next_number/2)
+      |> then(fn {_, board, draw} -> BingoBoard.score(board) * draw end)
+    end
+  end
+
+  defmodule Task2 do
+  end
+end
