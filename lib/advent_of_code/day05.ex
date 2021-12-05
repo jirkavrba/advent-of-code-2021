92b71f833b4e05361f0d96143651fbda8ab3038a
Jiří Vrba <jiri@vrba.dev>
2021-12-05 11:33:34 +0100

    Implement day 5, task 2 in elixir

--- lib/advent_of_code/day05.ex
+++ lib/advent_of_code/day05.ex
@@ -7,23 +7,31 @@ defmodule AdventOfCode.Day05 do
 
     defstruct x: 0, y: 0
 
+    defguard is_straight(x1, x2, y1, y2) when x1 == x2 or y1 == y2
+
     @spec new(list(integer())) :: t()
     def new([x, y]), do: %Location{x: x, y: y}
 
     @spec points_between(list(t())) :: list(t())
-    def points_between([%Location{x: x1, y: y1}, %Location{x: x2, y: y2}]) do
+    def points_between([%Location{x: x1, y: y1}, %Location{x: x2, y: y2}]) when is_straight(x1, x2, y1, y2)  do
       for x <- min(x1, x2) .. max(x1, x2),
           y <- min(y1, y2) .. max(y1, y2),
-          do: %Location{x: x, y: y}
+      do: %Location{x: x, y: y}
+    end
+
+    def points_between([%Location{x: x1, y: y1}, %Location{x: x2, y: y2}]) do
+      x1..x2
+      |> Enum.zip(y1..y2)
+      |> Enum.map(fn {x, y} -> %Location{x: x, y: y} end)
     end
 
     @spec is_straight_line(list(list(integer()))) :: boolean()
-    def is_straight_line([%Location{x: x1, y: y1}, %Location{x: x2, y: y2}]), do: x1 == x2 or y1 == y2
+    def is_straight_line([%Location{x: x1, y: y1}, %Location{x: x2, y: y2}]) when is_straight(x1, x2, y1, y2), do: true
     def is_straight_line(_), do: false
   end
 
-  @spec venture_to_points(String.t()) :: list(Location.t())
-  defp venture_to_points(line) do
+  @spec venture_to_points(String.t(), fun()) :: list(Location.t())
+  defp venture_to_points(line, filter \\ fn _ -> true end) do
     # The format is x1,y1 -> x2,y2
     ~r"(\d+),(\d+)\s+->\s+(\d+),(\d+)"
     |> Regex.run(line)
@@ -32,8 +40,16 @@ defmodule AdventOfCode.Day05 do
     |> Enum.chunk_every(2)
     |> Enum.map(&Location.new/1)
     |> Enum.chunk_every(2)
-    |> Enum.filter(&Location.is_straight_line/1)
+    |> Enum.filter(filter)
     |> Enum.flat_map(&Location.points_between/1)
+  end
+
+  @spec compute_venture_overlaps_excluding_diagonals(list(String.t())) :: integer()
+  def compute_venture_overlaps_excluding_diagonals(input) when is_list(input) do
+    input
+    |> Enum.flat_map(fn line -> venture_to_points(line, &Location.is_straight_line/1) end)
+    |> Enum.frequencies()
+    |> Enum.count(fn {_, count} -> count > 1 end)
   end
 
   @spec compute_venture_overlaps(list(String.t())) :: integer()
@@ -41,8 +57,7 @@ defmodule AdventOfCode.Day05 do
     input
     |> Enum.flat_map(&venture_to_points/1)
     |> Enum.frequencies()
-    |> Map.values()
-    |> Enum.count(&(&1 > 1))
+    |> Enum.count(fn {_, count} -> count > 1 end)
   end
 
 end
--- test/advent_of_code/day05_test.exs
+++ test/advent_of_code/day05_test.exs
@@ -5,36 +5,47 @@ defmodule AdventOfCode.Day05Test do
 
   test "task 1 example" do
     input = """
-      0,9 -> 5,9
-      8,0 -> 0,8
-      9,4 -> 3,4
-      2,2 -> 2,1
-      7,0 -> 7,4
-      6,4 -> 2,0
-      0,9 -> 2,9
-      3,4 -> 1,4
-      0,0 -> 8,8
-      5,5 -> 8,2
+    0,9 -> 5,9
+    8,0 -> 0,8
+    9,4 -> 3,4
+    2,2 -> 2,1
+    7,0 -> 7,4
+    6,4 -> 2,0
+    0,9 -> 2,9
+    3,4 -> 1,4
+    0,0 -> 8,8
+    5,5 -> 8,2
     """
     |> String.split("\n", trim: true)
 
-    assert Day05.compute_venture_overlaps(input) == 5
+    assert Day05.compute_venture_overlaps_excluding_diagonals(input) == 5
   end
 
   test "task 1 input" do
     input = Input.read_lines_from_file("priv/inputs/day05.txt")
-    assert Day05.compute_venture_overlaps(input) == 6005
+    assert Day05.compute_venture_overlaps_excluding_diagonals(input) == 6005
   end
 
   test "task 2 example" do
-    # input = [
-    # ]
+    input = """
+    0,9 -> 5,9
+    8,0 -> 0,8
+    9,4 -> 3,4
+    2,2 -> 2,1
+    7,0 -> 7,4
+    6,4 -> 2,0
+    0,9 -> 2,9
+    3,4 -> 1,4
+    0,0 -> 8,8
+    5,5 -> 8,2
+    """
+    |> String.split("\n", trim: true)
 
-    # assert Task2.___(input) == 0
+    assert Day05.compute_venture_overlaps(input) == 12
   end
 
   test "task 2 input" do
-    # input = Input.read_lines_from_file("priv/inputs/day05.txt")
-    # assert Task2.___(input) == 0
+    input = Input.read_lines_from_file("priv/inputs/day05.txt")
+    assert Day05.compute_venture_overlaps(input) == 23864
   end
 end
