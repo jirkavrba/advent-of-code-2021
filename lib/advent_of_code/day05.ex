e802fe9af57a8ae598aa30c58156dc4d8d255f0a
Jiří Vrba <jiri@vrba.dev>
2021-12-05 12:04:26 +0100

    Further day 5 solution cleanup

--- lib/advent_of_code/day05.ex
+++ lib/advent_of_code/day05.ex
@@ -1,55 +1,54 @@
 defmodule AdventOfCode.Day05 do
-  defmodule Location do
+  defmodule Point do
     @type t :: %__MODULE__{
-      x: integer(),
-      y: integer()
-    }
+            x: integer(),
+            y: integer()
+          }
 
     defstruct x: 0, y: 0
 
     defguard is_straight(x1, x2, y1, y2) when x1 == x2 or y1 == y2
 
     @spec new(list(integer())) :: t()
-    def new([x, y]), do: %Location{x: x, y: y}
+    def new([x, y]), do: %Point{x: x, y: y}
 
-    @spec points_between(list(t())) :: list(t())
-    def points_between([%Location{x: x1, y: y1}, %Location{x: x2, y: y2}]) when is_straight(x1, x2, y1, y2)  do
-      for x <- min(x1, x2) .. max(x1, x2),
-          y <- min(y1, y2) .. max(y1, y2),
-      do: %Location{x: x, y: y}
+    @spec points_between({t(), t()}) :: list(t())
+    def points_between({%Point{x: x1, y: y1}, %Point{x: x2, y: y2}}) when is_straight(x1, x2, y1, y2) do
+      for x <- x1..x2, y <- y1..y2, do: %Point{x: x, y: y}
     end
 
-    def points_between([%Location{x: x1, y: y1}, %Location{x: x2, y: y2}]) do
-      x1..x2
-      |> Enum.zip(y1..y2)
-      |> Enum.map(fn {x, y} -> %Location{x: x, y: y} end)
+    # Diagonals
+    def points_between({%Point{x: x1, y: y1}, %Point{x: x2, y: y2}}) do
+      for {x, y} <- Enum.zip(x1..x2, y1..y2), do: %Point{x: x, y: y}
     end
 
-    @spec is_straight_line(list(list(integer()))) :: boolean()
-    def is_straight_line([%Location{x: x1, y: y1}, %Location{x: x2, y: y2}]) when is_straight(x1, x2, y1, y2), do: true
+    def is_straight_line({%Point{x: x1, y: y1}, %Point{x: x2, y: y2}}) when is_straight(x1, x2, y1, y2), do: true
     def is_straight_line(_), do: false
   end
 
-  @spec venture_to_locations(String.t()) :: list(list(Location.t()))
+  @spec venture_to_locations(String.t()) :: {Point.t(), Point.t()}
   defp venture_to_locations(line) do
     # The format is x1,y1 -> x2,y2
     ~r"(\d+),(\d+)\s+->\s+(\d+),(\d+)"
     |> Regex.run(line)
-    |> Enum.drop(1) # The first element is the whole regex match
+    # The first element is the whole regex match
+    |> Enum.drop(1)
     |> Enum.map(&String.to_integer/1)
     |> Enum.chunk_every(2)
-    |> Enum.map(&Location.new/1)
+    |> Enum.map(&Point.new/1)
+    |> List.to_tuple()
   end
 
   @spec compute_venture_overlaps(list(String.t()), boolean()) :: integer()
   def compute_venture_overlaps(input, include_diagonals \\ false) when is_list(input) do
-    filter = fn line -> include_diagonals or Location.is_straight_line(line) end
+    filter = fn line -> include_diagonals or Point.is_straight_line(line) end
 
     input
     |> Enum.map(&venture_to_locations/1)
     |> Enum.filter(filter)
-    |> Enum.flat_map(&Location.points_between/1)
+    |> Enum.flat_map(&Point.points_between/1)
     |> Enum.frequencies()
-    |> Enum.count(fn {_, count} -> count > 1 end)
+    |> Map.values()
+    |> Enum.count(fn count -> count > 1 end)
   end
 end
