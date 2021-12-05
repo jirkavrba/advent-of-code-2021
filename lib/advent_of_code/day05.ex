c827d53b142a275d6a725ca2d24264b7e4278c02
Jiří Vrba <jiri@vrba.dev>
2021-12-05 12:19:50 +0100

    Day 5 cleanup

--- lib/advent_of_code/day05.ex
+++ lib/advent_of_code/day05.ex
@@ -1,52 +1,48 @@
 defmodule AdventOfCode.Day05 do
-  defmodule Point do
-    @type t :: %__MODULE__{
-            x: integer(),
-            y: integer()
+  defmodule Line do
+    @type t() :: %__MODULE__{
+            x1: integer(), x2: integer(),
+            y1: integer(), y2: integer()
           }
 
-    defstruct x: 0, y: 0
-
-    defguard is_straight(x1, x2, y1, y2) when x1 == x2 or y1 == y2
-
+    @enforce_keys [:x1, :x2, :y1, :y2]
+    defstruct [:x1, :x2, :y1, :y2]
     @spec new(list(integer())) :: t()
-    def new([x, y]), do: %Point{x: x, y: y}
+    def new([x1, y1, x2, y2]), do: %Line{x1: x1, x2: x2, y1: y1, y2: y2}
 
-    @spec points_between({t(), t()}) :: list(t())
-    def points_between({%Point{x: x1, y: y1}, %Point{x: x2, y: y2}}) when is_straight(x1, x2, y1, y2) do
-      for x <- x1..x2, y <- y1..y2, do: %Point{x: x, y: y}
+    @spec is_straight(t()) :: boolean()
+    def is_straight(%Line{x1: x1, x2: x2, y1: y1, y2: y2}) do
+      x1 == x2 or y1 == y2
     end
 
-    # Diagonals
-    def points_between({%Point{x: x1, y: y1}, %Point{x: x2, y: y2}}) do
-      for {x, y} <- Enum.zip(x1..x2, y1..y2), do: %Point{x: x, y: y}
+    @spec points(t()) :: {integer(), integer()}
+    def points(%Line{x1: x1, x2: x2, y1: y1, y2: y2} = line) do
+      cond do
+        is_straight(line) -> for x <- x1..x2, y <- y1..y2, do: {x, y}
+        true              -> for {x, y} <- Enum.zip(x1..x2, y1..y2), do: {x, y}
+      end
     end
-
-    def is_straight_line({%Point{x: x1, y: y1}, %Point{x: x2, y: y2}}) when is_straight(x1, x2, y1, y2), do: true
-    def is_straight_line(_), do: false
   end
 
-  @spec venture_to_locations(String.t()) :: {Point.t(), Point.t()}
-  defp venture_to_locations(line) do
+  @spec venture_to_line(String.t()) :: Line.t()
+  defp venture_to_line(line) do
     # The format is x1,y1 -> x2,y2
+    # The first element is the whole regex match
     ~r"(\d+),(\d+)\s+->\s+(\d+),(\d+)"
     |> Regex.run(line)
-    # The first element is the whole regex match
     |> Enum.drop(1)
     |> Enum.map(&String.to_integer/1)
-    |> Enum.chunk_every(2)
-    |> Enum.map(&Point.new/1)
-    |> List.to_tuple()
+    |> Line.new()
   end
 
   @spec compute_venture_overlaps(list(String.t()), boolean()) :: integer()
   def compute_venture_overlaps(input, include_diagonals \\ false) when is_list(input) do
-    filter = fn line -> include_diagonals or Point.is_straight_line(line) end
+    filter = fn line -> include_diagonals or Line.is_straight(line) end
 
     input
-    |> Enum.map(&venture_to_locations/1)
+    |> Enum.map(&venture_to_line/1)
     |> Enum.filter(filter)
-    |> Enum.flat_map(&Point.points_between/1)
+    |> Enum.flat_map(&Line.points/1)
     |> Enum.frequencies()
     |> Map.values()
     |> Enum.count(fn count -> count > 1 end)
