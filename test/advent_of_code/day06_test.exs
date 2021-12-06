79268cb2d0e069b97220d90697137d1839e14a81
Jiří Vrba <jiri@vrba.dev>
2021-12-06 09:54:34 +0100

    Implement day 6, task 2 in elixir

--- test/advent_of_code/day06_test.exs
+++ test/advent_of_code/day06_test.exs
@@ -14,19 +14,19 @@ defmodule AdventOfCode.Day06Test do
     |> Input.read_lines_from_file()
     |> Enum.at(0)
 
-    assert Day06.compute_number_of_lanternfish_after(input) == 0
+    assert Day06.compute_number_of_lanternfish_after(input) == 379114
   end
 
   test "task 2 example" do
-    # input = """
-    # """
-    # |> String.split("\n", trim: true)
-
-    # assert Day06.__task2__(input) == 0
+    input = "3,4,3,1,2"
+    assert Day06.compute_number_of_lanternfish_after(input, 256) == 26984457539
   end
 
   test "task 2 input" do
-    # input = Input.read_lines_from_file("priv/inputs/day06.txt")
-    # assert Day06.__task2__(input) == 0
+    input = "priv/inputs/day06.txt"
+    |> Input.read_lines_from_file()
+    |> Enum.at(0)
+
+    assert Day06.compute_number_of_lanternfish_after(input, 256) == 1702631502303
   end
 end
