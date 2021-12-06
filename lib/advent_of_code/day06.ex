17bb9d8d323180202e9970194c4c17deb3801a3e
Jiří Vrba <jiri@vrba.dev>
2021-12-06 09:52:07 +0100

    Implement day 6, task 1 in elixir

--- lib/advent_of_code/day06.ex
+++ lib/advent_of_code/day06.ex
@@ -1,11 +1,31 @@
 defmodule AdventOfCode.Day06 do
+  @spec compute_number_of_lanternfish_after(String.t(), non_neg_integer()) :: non_neg_integer()
+  def compute_number_of_lanternfish_after(initial, days \\ 80) when is_integer(days) do
+    initial =
+      initial
+      |> String.trim()
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.frequencies()
 
-  def __task1__(input) do
-    :ok
+    reducer = fn _day, fish ->
+      fish
+      |> Enum.flat_map(fn {0, v} -> [{:new, v}, {8, v}]; {k, v} -> [{k - 1 , v}] end)
+      |> Map.new()
+      |> then(fn map ->
+        map
+        |> Map.put(6, Map.get(map, :new, 0) + Map.get(map, 6, 0))
+        |> Map.delete(:new)
+      end)
+    end
+
+    1..days
+    |> Enum.reduce(initial, reducer)
+    |> Enum.map(fn {_, v} -> v end)
+    |> Enum.sum()
   end
 
   def __task2__(input) do
     :ok
   end
-
 end
--- test/advent_of_code/day06_test.exs
+++ test/advent_of_code/day06_test.exs
@@ -4,28 +4,29 @@ defmodule AdventOfCode.Day06Test do
   alias AdventOfCode.Day06
 
   test "task 1 example" do
-    input = """
-    """ 
-    |> String.split("\n", trim: true)
+    input = "3,4,3,1,2"
 
-    assert Day06.__task1__(input) == 0
+    assert Day06.compute_number_of_lanternfish_after(input) == 5934
   end
 
   test "task 1 input" do
-    input = Input.read_lines_from_file("priv/inputs/day06.txt")
-    assert Task1.__task1__(input) == 0
+    input = "priv/inputs/day06.txt"
+    |> Input.read_lines_from_file()
+    |> Enum.at(0)
+
+    assert Day06.compute_number_of_lanternfish_after(input) == 0
   end
 
   test "task 2 example" do
-    input = """
-    """
-    |> String.split("\n", trim: true)
+    # input = """
+    # """
+    # |> String.split("\n", trim: true)
 
-    assert Day06.__task2__(input) == 0
+    # assert Day06.__task2__(input) == 0
   end
 
   test "task 2 input" do
-    input = Input.read_lines_from_file("priv/inputs/day06.txt")
-    assert Day06.__task2__(input) == 0
+    # input = Input.read_lines_from_file("priv/inputs/day06.txt")
+    # assert Day06.__task2__(input) == 0
   end
 end
