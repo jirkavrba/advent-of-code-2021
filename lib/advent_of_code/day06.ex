7f4185b59e6dfaee1ea08524795ec5f5e20d83f2
Jiří Vrba <jiri@vrba.dev>
2021-12-06 12:38:13 +0100

    Refactor day 6 solution

--- lib/advent_of_code/day06.ex
+++ lib/advent_of_code/day06.ex
@@ -1,32 +1,31 @@
 defmodule AdventOfCode.Day06 do
-
-  @lifetime_create 8
-  @lifetime_reset 6
-
   @spec compute_number_of_lanternfish_after(String.t(), non_neg_integer()) :: non_neg_integer()
-  def compute_number_of_lanternfish_after(initial, days \\ 80) when is_integer(days) do
+  def compute_number_of_lanternfish_after(input, days \\ 80) when is_integer(days) do
     initial =
-      initial
+      input
       |> String.trim()
       |> String.split(",")
       |> Enum.map(&String.to_integer/1)
       |> Enum.frequencies()
 
-    reducer = fn _day, fish ->
-      fish
-      |> Enum.flat_map(fn {0, v} -> [{:new, v}, {@lifetime_create, v}]; {k, v} -> [{k - 1 , v}] end)
-      |> Map.new()
-      |> then(fn map ->
-        map
-        |> Map.put(6, Map.get(map, :new, 0) + Map.get(map, @lifetime_reset, 0))
-        |> Map.delete(:new)
-      end)
+    import Map, only: [put: 3, get: 3, delete: 2]
+
+    shift_lifetimes = fn
+      {0, v} -> [{:new, v}, {8, v}]
+      {k, v} -> [{k - 1, v}]
     end
 
     1..days
-    |> Enum.reduce(initial, reducer)
+    |> Enum.reduce(initial, fn _, fish ->
+      fish
+      |> Enum.flat_map(shift_lifetimes)
+      |> Map.new()
+      |> then(fn map -> map
+        |> put(6, get(map, :new, 0) + get(map, 6, 0))
+        |> delete(:new)
+      end)
+    end)
     |> Map.values()
     |> Enum.sum()
   end
-
 end
