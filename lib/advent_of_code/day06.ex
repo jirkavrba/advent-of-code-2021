75f117d0023d1aa057ba5b55a26cb10524de7e9e
Jiří Vrba <jiri@vrba.dev>
2021-12-06 10:47:02 +0100

    Use module attribute instead of hardcoded values

--- lib/advent_of_code/day06.ex
+++ lib/advent_of_code/day06.ex
@@ -1,4 +1,8 @@
 defmodule AdventOfCode.Day06 do
+
+  @lifetime_create 8
+  @lifetime_reset 6
+
   @spec compute_number_of_lanternfish_after(String.t(), non_neg_integer()) :: non_neg_integer()
   def compute_number_of_lanternfish_after(initial, days \\ 80) when is_integer(days) do
     initial =
@@ -10,18 +14,19 @@ defmodule AdventOfCode.Day06 do
 
     reducer = fn _day, fish ->
       fish
-      |> Enum.flat_map(fn {0, v} -> [{:new, v}, {8, v}]; {k, v} -> [{k - 1 , v}] end)
+      |> Enum.flat_map(fn {0, v} -> [{:new, v}, {@lifetime_create, v}]; {k, v} -> [{k - 1 , v}] end)
       |> Map.new()
       |> then(fn map ->
         map
-        |> Map.put(6, Map.get(map, :new, 0) + Map.get(map, 6, 0))
+        |> Map.put(6, Map.get(map, :new, 0) + Map.get(map, @lifetime_reset, 0))
         |> Map.delete(:new)
       end)
     end
 
     1..days
     |> Enum.reduce(initial, reducer)
-    |> Enum.map(fn {_, v} -> v end)
+    |> Map.values()
     |> Enum.sum()
   end
+
 end
