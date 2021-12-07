e1edc2f86c6533875223c3cd913d427805084d14
Jiří Vrba <jiri@vrba.dev>
2021-12-07 10:33:54 +0100

    Implement naive solution to day 7, task 2 in elixir

--- lib/advent_of_code/day07.ex
+++ lib/advent_of_code/day07.ex
@@ -1,24 +1,32 @@
 defmodule AdventOfCode.Day07 do
 
-  defp fuel_consumed(center, positions) do
+  @spec fuel_consumed(integer(), list(integer()), :constant | :linear)  :: integer()
+
+  # 1 -> 1 -> 1 -> 1 -> 1
+  defp fuel_consumed(center, positions, :constant) do
     positions
     |> Enum.map(fn x -> abs(center - x) end)
     |> Enum.sum()
   end
 
-  def compute_fuel_consumption(input) do
+  # 1 -> 2 -> 3 -> 4 -> 5
+  defp fuel_consumed(center, positions, :linear) do
+    positions
+    |> Enum.map(fn x -> abs(center - x) end)
+    |> Enum.map(fn x -> (x/2) * (x + 1) end)
+    |> Enum.map(&trunc/1)
+    |> Enum.sum()
+  end
+
+  @spec compute_fuel_consumption(String.t(), :constant | :linear) :: integer()
+  def compute_fuel_consumption(input, consumption_acceleration) do
     positions = input
     |> String.split(",")
     |> Enum.map(&String.to_integer/1)
     |> Enum.sort()
 
     List.first(positions) .. List.last(positions)
-    |> Enum.map(fn position -> fuel_consumed(position, positions) end)
+    |> Enum.map(fn position -> fuel_consumed(position, positions, consumption_acceleration) end)
     |> Enum.min()
   end
-
-  def __task2__(input) do
-    :ok
-  end
-
 end
--- test/advent_of_code/day07_test.exs
+++ test/advent_of_code/day07_test.exs
@@ -5,7 +5,7 @@ defmodule AdventOfCode.Day07Test do
 
   test "task 1 example" do
     input = "16,1,2,0,4,2,7,1,2,14"
-    assert Day07.compute_fuel_consumption(input) == 37
+    assert Day07.compute_fuel_consumption(input, :constant) == 37
   end
 
   test "task 1 input" do
@@ -14,19 +14,20 @@ defmodule AdventOfCode.Day07Test do
       |> Input.read_lines_from_file()
       |> Enum.at(0)
 
-    assert Day07.compute_fuel_consumption(input) == 352997
+    assert Day07.compute_fuel_consumption(input, :constant) == 352997
   end
 
   test "task 2 example" do
-    # input = """
-    # """
-    # |> String.split("\n", trim: true)
-
-    # assert Day07.__task2__(input) == 0
+    input = "16,1,2,0,4,2,7,1,2,14"
+    assert Day07.compute_fuel_consumption(input, :linear) == 168
   end
 
   test "task 2 input" do
-    # input = Input.read_lines_from_file("priv/inputs/day07.txt")
-    # assert Day07.__task2__(input) == 0
+    input =
+      "priv/inputs/day07.txt"
+      |> Input.read_lines_from_file()
+      |> Enum.at(0)
+
+    assert Day07.compute_fuel_consumption(input, :linear) == 101571302
   end
 end
