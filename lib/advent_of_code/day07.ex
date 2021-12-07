f3d4e46f67069442e12518795b1505c611be4f98
Jiří Vrba <jiri@vrba.dev>
2021-12-07 10:12:20 +0100

    Naive solution of day 7, task 1 in elixir

--- lib/advent_of_code/day07.ex
+++ lib/advent_of_code/day07.ex
@@ -1,7 +1,20 @@
 defmodule AdventOfCode.Day07 do
 
-  def __task1__(input) do
-    :ok
+  defp fuel_consumed(center, positions) do
+    positions
+    |> Enum.map(fn x -> abs(center - x) end)
+    |> Enum.sum()
+  end
+
+  def compute_fuel_consumption(input) do
+    positions = input
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sort()
+
+    List.first(positions) .. List.last(positions)
+    |> Enum.map(fn position -> fuel_consumed(position, positions) end)
+    |> Enum.min()
   end
 
   def __task2__(input) do
--- test/advent_of_code/day07_test.exs
+++ test/advent_of_code/day07_test.exs
@@ -4,28 +4,29 @@ defmodule AdventOfCode.Day07Test do
   alias AdventOfCode.Day07
 
   test "task 1 example" do
-    input = """
-    """ 
-    |> String.split("\n", trim: true)
-
-    assert Day07.__task1__(input) == 0
+    input = "16,1,2,0,4,2,7,1,2,14"
+    assert Day07.compute_fuel_consumption(input) == 37
   end
 
   test "task 1 input" do
-    input = Input.read_lines_from_file("priv/inputs/day07.txt")
-    assert Task1.__task1__(input) == 0
+    input =
+      "priv/inputs/day07.txt"
+      |> Input.read_lines_from_file()
+      |> Enum.at(0)
+
+    assert Day07.compute_fuel_consumption(input) == 352997
   end
 
   test "task 2 example" do
-    input = """
-    """
-    |> String.split("\n", trim: true)
+    # input = """
+    # """
+    # |> String.split("\n", trim: true)
 
-    assert Day07.__task2__(input) == 0
+    # assert Day07.__task2__(input) == 0
   end
 
   test "task 2 input" do
-    input = Input.read_lines_from_file("priv/inputs/day07.txt")
-    assert Day07.__task2__(input) == 0
+    # input = Input.read_lines_from_file("priv/inputs/day07.txt")
+    # assert Day07.__task2__(input) == 0
   end
 end
