e3a9eeb6ab4f6bc60c45d7cb9ed464d009348cd3
Jiří Vrba <jiri@vrba.dev>
2021-12-08 12:31:33 +0100

    WIP

--- lib/advent_of_code/day08.ex
+++ lib/advent_of_code/day08.ex
@@ -1,15 +1,52 @@
 defmodule AdventOfCode.Day08 do
 
+  @spec solve_first_task(list(String.t())) :: non_neg_integer()
   def solve_first_task(input) when is_list(input) do
     input
-    |> Enum.map(fn line -> String.split(line, "|", trim: true) end)
-    |> Enum.flat_map(fn [_, output] -> String.split(output, " ", trim: true) end)
+    |> parse_input()
+    |> Enum.flat_map(fn {_, output} -> output end)
     |> Enum.map(&String.length/1)
     |> Enum.count(&(&1 in [2, 3, 4, 7]))
   end
 
-  def __task2__(input) do
-    :ok
+  @spec solve_second_task(list(String.t())) :: non_neg_integer()
+  def solve_second_task(input) do
+    input
+    |> parse_input()
+    |> Enum.map(fn {input, output} -> {translate_segments(input), output} end)
+    |> Enum.map(fn {translations, output} -> for i <- output, do: translations[i] end)
   end
 
+  @spec parse_input(list(String.t())) :: list({list(String.t()), list(String.t())})
+  defp parse_input(input) when is_list(input) do
+    input
+    |> Enum.map(fn line -> String.split(line, "|", trim: true) end)
+    |> Enum.map(fn parts -> parts
+      |> Enum.map(fn part -> String.split(part, " ", trim: true) end)
+      |> List.to_tuple()
+    end)
+  end
+
+  @spec translate_segments(list(String.t())) :: %{char() => char()}
+  defp translate_segments(input) when is_list(input) do
+    segments = [
+      #a  b  c  d  e  f  g
+      [1, 1, 1, 0, 1, 1, 1], # 0
+      [0, 0, 1, 0, 0, 1, 0], # 1
+      [1, 0, 1, 1, 1, 0, 1], # 2
+      [1, 0, 1, 1, 0, 1, 1], # 3
+      [0, 1, 1, 1, 0, 1, 0], # 4
+      [1, 1, 0, 1, 0, 1, 1], # 5
+      [1, 1, 0, 1, 1, 1, 1], # 6
+      [1, 0, 1, 0, 0, 1, 0], # 7
+      [1, 1, 1, 1, 1, 1, 1], # 8
+      [1, 1, 1, 1, 0, 1, 1], # 9
+    ]
+
+    string_to_bits = fn string -> ?a..?b |> Enum.map(&(String.contains?(string, &1))) end
+
+    input
+    |> Enum.map(string_to_bits)
+    |> IO.inspect()
+  end
 end
--- test/advent_of_code/day08_test.exs
+++ test/advent_of_code/day08_test.exs
@@ -25,12 +25,28 @@ defmodule AdventOfCode.Day08Test do
     assert Day08.solve_first_task(input) == 247
   end
 
-  test "task 2 example" do
-    # input = """
-    # """
-    # |> String.split("\n", trim: true)
 
-    # assert Day08.__task2__(input) == 0
+  test "task 2 short example" do
+    input = ["acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"]
+    assert Day08.solve_second_task(input) == 5353
+  end
+
+  test "task 2 long example" do
+    input = """
+    be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+    edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+    fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+    fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+    aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+    fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+    dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+    bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+    egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+    gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+    """
+    |> String.split("\n", trim: true)
+
+    assert Day08.solve_second_task(input) == 0
   end
 
   test "task 2 input" do
