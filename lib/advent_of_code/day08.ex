7b808fd77ea2a3eda02b0312020fbef1e1434558
Jiří Vrba <jiri@vrba.dev>
2021-12-08 11:20:04 +0100

    Solve day 8, part 1 in elixir

--- lib/advent_of_code/day08.ex
+++ lib/advent_of_code/day08.ex
@@ -1,7 +1,11 @@
 defmodule AdventOfCode.Day08 do
 
-  def __task1__(input) do
-    :ok
+  def solve_first_task(input) when is_list(input) do
+    input
+    |> Enum.map(fn line -> String.split(line, "|", trim: true) end)
+    |> Enum.flat_map(fn [_, output] -> String.split(output, " ", trim: true) end)
+    |> Enum.map(&String.length/1)
+    |> Enum.count(&(&1 in [2, 3, 4, 7]))
   end
 
   def __task2__(input) do
