f8d89ea349cb9c6ecb418e50e27a7e9198bffa2e
Jiří Vrba <jiri@vrba.dev>
2021-12-09 09:22:41 +0100

    wip

--- lib/advent_of_code/day09.ex
+++ lib/advent_of_code/day09.ex
@@ -1,11 +1,27 @@
 defmodule AdventOfCode.Day09 do
 
-  def __task1__(input) do
-    :ok
+  @spec find_sum_of_low_points(list(String.t())) :: integer()
+  def find_sum_of_low_points(input) when is_list(input) do
+    width = input |> Enum.at(0) |> String.length()
+    height = input |> Enum.count()
+
+    matrix = input
+    |> Enum.flat_map(&String.graphemes/1)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.chunk_every(width)
+    |> find_low_points(width, height)
   end
 
-  def __task2__(input) do
-    :ok
+  defp neighbours(matrix, x, y, width, height) do
+    c = [matrix |> Enum.at(y) |> Enum.at(x)]
+
+    dx = for i <- max(x - 1, 0) .. min(x + 1, width - 1), do: matrix |> Enum.at(y) |> Enum.at(i)
+    dy = for i <- max(y - 1, 0) .. min(y + 1, height - 1), do: matrix |> Enum.at(i) |> Enum.at(i)
+
+    dx ++ dy -- c -- c
   end
 
+  defp find_low_points(matrix, width, height) do
+    neighbours(matrix, 0, 0, width, height)
+  end
 end
--- test/advent_of_code/day09_test.exs
+++ test/advent_of_code/day09_test.exs
@@ -5,27 +5,32 @@ defmodule AdventOfCode.Day09Test do
 
   test "task 1 example" do
     input = """
-    """ 
-    |> String.split("\n", trim: true)
-
-    assert Day09.__task1__(input) == 0
-  end
-
-  test "task 1 input" do
-    input = Input.read_lines_from_file("priv/inputs/day09.txt")
-    assert Day09.__task1__(input) == 0
-  end
-
-  test "task 2 example" do
-    input = """
+    2199943210
+    3987894921
+    9856789892
+    8767896789
+    9899965678
     """
     |> String.split("\n", trim: true)
 
-    assert Day09.__task2__(input) == 0
+    assert Day09.find_sum_of_low_points(input) == 15
+  end
+
+  test "task 1 input" do
+    # input = Input.read_lines_from_file("priv/inputs/day09.txt")
+    # assert Day09.find_sum_of_low_points(input) == 0
+  end
+
+  test "task 2 example" do
+    # input = """
+    # """
+    # |> String.split("\n", trim: true)
+
+    # assert Day09.__task2__(input) == 0
   end
 
   test "task 2 input" do
-    input = Input.read_lines_from_file("priv/inputs/day09.txt")
-    assert Day09.__task2__(input) == 0
+    # input = Input.read_lines_from_file("priv/inputs/day09.txt")
+    # assert Day09.__task2__(input) == 0
   end
 end
