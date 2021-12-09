ab8fda07aab3f829711950912072b34838b9a648
Jiří Vrba <jiri@vrba.dev>
2021-12-09 18:58:39 +0100

    Implement day 8, task 2 in elixir

--- lib/advent_of_code/day08.ex
+++ lib/advent_of_code/day08.ex
@@ -1,5 +1,15 @@
 defmodule AdventOfCode.Day08 do
 
+  @spec parse_input(list(String.t())) :: list({list(String.t()), list(String.t())})
+  defp parse_input(input) when is_list(input) do
+    input
+    |> Enum.map(fn line -> String.split(line, "|", trim: true) end)
+    |> Enum.map(fn parts -> parts
+      |> Enum.map(fn part -> String.split(part, " ", trim: true) end)
+      |> List.to_tuple()
+    end)
+  end
+
   @spec solve_first_task(list(String.t())) :: non_neg_integer()
   def solve_first_task(input) when is_list(input) do
     input
@@ -13,47 +23,67 @@ defmodule AdventOfCode.Day08 do
   def solve_second_task(input) do
     input
     |> parse_input()
-    |> Enum.map(fn {input, output} -> {translate_segments(input), output} end)
-    |> Enum.map(fn {translations, output} -> for i <- output, do: translations[i] end)
+    |> Enum.map(fn {input, output} -> {solve_segment_wiring(input), output} end)
+    |> Enum.map(fn {wiring, output} -> for digit <- output, do: translate_digit(wiring, digit) end)
+    |> Enum.map(&Integer.undigits/1)
+    |> Enum.sum()
   end
 
-  @spec parse_input(list(String.t())) :: list({list(String.t()), list(String.t())})
-  defp parse_input(input) when is_list(input) do
-    input
-    |> Enum.map(fn line -> String.split(line, "|", trim: true) end)
-    |> Enum.map(fn parts -> parts
-      |> Enum.map(fn part -> String.split(part, " ", trim: true) end)
-      |> List.to_tuple()
-    end)
-  end
-
-  @spec translate_segments(list(String.t())) :: %{char() => char()}
-  defp translate_segments(input) when is_list(input) do
-    segments = [
-      #a  b  c  d  e  f  g
-      [:a, :b, :c,     :e, :f, :g], # 0
-      [        :c,         :f    ], # 1
-      [:a,     :c, :d, :e,     :g], # 2
-      [:a,     :c, :d,     :f, :g], # 3
-      [    :b, :c, :d,     :f    ], # 4
-      [:a, :b,     :d,     :f, :g], # 5
-      [:a, :b,     :d, :e, :f, :g], # 6
-      [:a,     :c,         :f    ], # 7
-      [:a, :b, :c, :d, :e, :f, :g], # 8
-      [:a, :b, :c, :d,     :f, :g] # 9
+  @spec translate_digit(map(), String.t()) :: integer()
+  defp translate_digit(wiring, digit) do
+    digits = [
+      "abcefg",  #0
+      "cf",      #1
+      "acdeg",   #2
+      "acdfg",   #3
+      "bcdf",    #4
+      "abdfg",   #5
+      "abdefg",  #6
+      "acf",     #7
+      "abcdefg", #8
+      "abcdfg",  #9
     ]
 
+    segments = digit
+    |> String.graphemes()
+    |> Enum.map(&String.to_atom/1)
+    |> Enum.map(fn key -> wiring[key] end)
+    |> Enum.sort()
+    |> Enum.join("")
+
+    digits
+    |> Enum.find_index(&(&1 == segments))
+  end
+
+  @spec solve_segment_wiring(list(String.t())) :: Keyword.t()
+  defp solve_segment_wiring(input) when is_list(input) do
     string_to_bits = fn string ->
       string
       |> String.graphemes()
-      |> Enum.sort()
       |> Enum.map(&String.to_atom/1)
-      |> Enum.filter(&(&1 in [:a, :b, :c, :d, :e, :f, :g]))
+      |> Enum.sort()
     end
 
     sets = input
     |> Enum.map(string_to_bits)
     |> Enum.uniq()
 
+    one   = Enum.find(sets, fn s -> length(s) == 2 end)
+    four  = Enum.find(sets, fn s -> length(s) == 4 end)
+    seven = Enum.find(sets, fn s -> length(s) == 3 end)
+    eight = Enum.find(sets, fn s -> length(s) == 7 end)
+    nine  = Enum.find(sets, fn s -> length(s) == 6 and length(s -- four) == 2 end)
+    six   = Enum.find(sets, fn s -> length(s) == 6 and length(s -- seven) == 4 end)
+    zero  = Enum.find(sets, fn s -> length(s) == 6 and length(s -- four) != 2 and length(s -- seven) != 4 end)
+
+    [a] = seven -- one
+    [e] = eight -- nine
+    [d] = eight -- zero
+    [c] = eight -- six
+    [f] = one -- [c]
+    [b] = four -- [c, d, f]
+    [g] = eight -- [a, b, c, d, e, f]
+
+    Enum.map([a: a, b: b, c: c, d: d, e: e, f: f, g: g], fn {k, v} -> {v, k} end)
   end
 end
--- test/advent_of_code/day08_test.exs
+++ test/advent_of_code/day08_test.exs
@@ -27,30 +27,30 @@ defmodule AdventOfCode.Day08Test do
 
 
   test "task 2 short example" do
-    # input = ["acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"]
-    # assert Day08.solve_second_task(input) == 5353
+    input = ["acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"]
+    assert Day08.solve_second_task(input) == 5353
   end
 
   test "task 2 long example" do
-    # input = """
-    # be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
-    # edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
-    # fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
-    # fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
-    # aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
-    # fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
-    # dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
-    # bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
-    # egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
-    # gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
-    # """
-    # |> String.split("\n", trim: true)
+    input = """
+    be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+    edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+    fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+    fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+    aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+    fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+    dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+    bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+    egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+    gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+    """
+    |> String.split("\n", trim: true)
 
-    # assert Day08.solve_second_task(input) == 0
+    assert Day08.solve_second_task(input) == 61229
   end
 
   test "task 2 input" do
-    # input = Input.read_lines_from_file("priv/inputs/day08.txt")
-    # assert Day08.__task2__(input) == 0
+    input = Input.read_lines_from_file("priv/inputs/day08.txt")
+    assert Day08.solve_second_task(input) == 933305
   end
 end
