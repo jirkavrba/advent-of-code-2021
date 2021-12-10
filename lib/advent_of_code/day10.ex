3b3720408cfcc78e3c788d5f6adc79ad16a7cbd5
Jiří Vrba <jiri@vrba.dev>
2021-12-10 09:48:12 +0100

    Implement day 10, task 1

--- lib/advent_of_code/day10.ex
+++ lib/advent_of_code/day10.ex
@@ -1,11 +1,45 @@
 defmodule AdventOfCode.Day10 do
-
-  def __task1__(input) do
-    :ok
+  @spec compute_syntax_error_score(list(String.t())) :: integer()
+  def compute_syntax_error_score(input) when is_list(input) do
+    input
+    |> Enum.map(&String.to_charlist/1)
+    |> Enum.map(&find_errors/1)
+    |> Enum.map(&score_first_error/1)
+    |> Enum.sum()
   end
 
-  def __task2__(input) do
-    :ok
+  defguard is_opening(char) when char in [?(, ?[, ?{, ?<]
+  defguard is_closing(char) when char in [?), ?], ?}, ?>]
+
+  defguard matches(opening, closing)
+           when {opening, closing} in [
+                  {?(, ?)},
+                  {?[, ?]},
+                  {?{, ?}},
+                  {?<, ?>}
+                ]
+
+  @spec find_errors(charlist()) :: charlist()
+  defp find_errors(input) when is_list(input) do
+    reducer = fn
+      char, {stack, errors} when is_opening(char)      -> {[char | stack], errors}
+      char, {stack, errors} when is_closing(char)      ->
+        case stack do
+          []                                     -> {stack, errors ++ [char]}
+          [head | rest] when matches(head, char) -> {rest,  errors}
+          [_ | rest]                             -> {rest,  errors ++ [char]}
+        end
+    end
+
+    {_, errors} = Enum.reduce(input, {[], []}, reducer)
+
+    errors
   end
 
+  @spec score_first_error(charlist()) :: integer()
+  defp score_first_error([]), do: 0
+  defp score_first_error([?) | _]), do: 3
+  defp score_first_error([?] | _]), do: 57
+  defp score_first_error([?} | _]), do: 1197
+  defp score_first_error([?> | _]), do: 25137
 end
--- test/advent_of_code/day10_test.exs
+++ test/advent_of_code/day10_test.exs
@@ -5,27 +5,37 @@ defmodule AdventOfCode.Day10Test do
 
   test "task 1 example" do
     input = """
-    """ 
+    [({(<(())[]>[[{[]{<()<>>
+    [(()[<>])]({[<{<<[]>>(
+    {([(<{}[<>[]}>{[]{[(<()>
+    (((({<>}<{<{<>}{[]{[]{}
+    [[<[([]))<([[{}[[()]]]
+    [{[{({}]{}}([{[{{{}}([]
+    {<[[]]>}<{[{[{[]{()[[[]
+    [<(<(<(<{}))><([]([]()
+    <{([([[(<>()){}]>(<<{{
+    <{([{{}}[<[[[<>{}]]]>[]]
+    """
     |> String.split("\n", trim: true)
 
-    assert Day10.__task1__(input) == 0
+    assert Day10.compute_syntax_error_score(input) == 26397
   end
 
   test "task 1 input" do
     input = Input.read_lines_from_file("priv/inputs/day10.txt")
-    assert Day10.__task1__(input) == 0
+    assert Day10.compute_syntax_error_score(input) == 369105
   end
 
   test "task 2 example" do
-    input = """
-    """
-    |> String.split("\n", trim: true)
+    # input = """
+    # """
+    # |> String.split("\n", trim: true)
 
-    assert Day10.__task2__(input) == 0
+    # assert Day10.__task2__(input) == 0
   end
 
   test "task 2 input" do
-    input = Input.read_lines_from_file("priv/inputs/day10.txt")
-    assert Day10.__task2__(input) == 0
+    # input = Input.read_lines_from_file("priv/inputs/day10.txt")
+    # assert Day10.__task2__(input) == 0
   end
 end
